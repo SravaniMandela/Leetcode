b924c374a7eaf9b67702494a018777598ffed06c
sravani <sravanimandela140@gmail.com>
2025-07-11 14:06:23 +0530

    longest common prefix

--- longestCommonPrefix.py
+++ longestCommonPrefix.py
@@ -1,14 +1,14 @@
 class Solution(object):
     def longestCommonPrefix(self, strs):
-        st=""
+        st = ""
         for i in range(len(strs[0])): 
-            j=strs[0][i]            
+            j = strs[0][i]            
             for k in range(len(strs)):
-                if i>=len(strs[k]) or j!=strs[k][i]:
-                    print(st)
-                    return
-            st=st+j
-        print(st)
-strs = ["flower","flow","flight"]
+                if i >= len(strs[k]) or j != strs[k][i]:
+                    return st
+            st = st + j
+        return st
+
+strs = ["flower", "flow", "flight"]
 solution = Solution()
-solution.longestCommonPrefix(strs)
+print(solution.longestCommonPrefix(strs))
