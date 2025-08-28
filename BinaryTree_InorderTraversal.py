f2c4cd54287b12b5cc7a1747645d51f202417cdf
sravani <sravanimandela140@gmail.com>
2025-08-28 11:43:34 +0530

    binary tree inorder traversal

---
+++ BinaryTree_InorderTraversal.py
@@ -0,0 +1,13 @@
+#logic:left-node-right
+class Solution(object):
+    def inorderTraversal(self, root):
+        result = []
+        def inorder(node):
+            if not node:
+                return
+            inorder(node.left)    
+            result.append(node  .val) 
+            inorder(node.right)    
+        inorder(root)
+        return result
+
