0d4ffc389e68be99f845ac68eece7f9e10a366dd
sravani <sravanimandela140@gmail.com>
2025-08-28 11:58:46 +0530

    modified input

--- BinaryTree_InorderTraversal.py
+++ BinaryTree_InorderTraversal.py
@@ -1,4 +1,10 @@
 #logic:left-node-right
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
 class Solution(object):
     def inorderTraversal(self, root):
         result = []
@@ -6,8 +12,13 @@ class Solution(object):
             if not node:
                 return
             inorder(node.left)    
-            result.append(node  .val) 
+            result.append(node.val) 
             inorder(node.right)    
         inorder(root)
         return result
-
+        
+root = TreeNode(1)
+root.right = TreeNode(2)
+root.right.left = TreeNode(3)      #tree type input 
+sol=Solution()
+print(sol.inorderTraversal(root))
