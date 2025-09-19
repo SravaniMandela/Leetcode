34e5430c26cb1ddcb2e890274de63546d2a8c215
sravani <sravanimandela140@gmail.com>
2025-09-19 08:53:08 +0530

    modified prices

--- bestTimetoButandSellStock.py
+++ bestTimetoButandSellStock.py
@@ -2,14 +2,14 @@ class Solution(object):
     def maxProfit(self, prices):
         min=prices[0]
         max_profit=0
-        max_p=[]
         for i in range(1,len(prices)):
             if(prices[i]<min):
                 min=prices[i]
             else:
-                max_profit=prices[i]-min
-                max_p.append(max_profit)
-        return max(max_p)
+                profit = prices[i] - min
+                if profit > max_profit:
+                    max_profit = profit
+        return max_profit
                 
 prices=[7,1,5,3,6,4]
 sol = Solution()
