444870d27f35dd8ef7b661cf67063be5a0d0dff6
sravani <sravanimandela140@gmail.com>
2026-03-12 10:44:14 +0530

    rev of a string

--- reverseString.py
+++ reverseString.py
@@ -3,11 +3,9 @@ class Solution(object):
         rev=[]
         for i in s:
             rev=[i]+rev
-        for i in range(len(s)):
-            s[i] = rev[i]
+        s[:]=rev
         return s
-            
         
-s = ["h","e","l","l","o"]        
+s = ["h","e","l","l","o"]
 sol=Solution()
 print(sol.reverseString(s))
